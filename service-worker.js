938a45399be50f5f798e08eccf769145a7449d0d
lumenhear <artdigi804@gmail.com>
2025-10-22 17:54:03 +0530

    Add service worker for caching resources

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'lumen-v1';
+const urlsToCache = [
+  '/',
+  '/styles.css',
+  '/app.js',
+  '/assets/icon-192.png',
+  '/assets/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
